b77dd12b4ae5e306d5201e819362e09863bfee4f
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-04-14 19:33:05 -0400

    add data seeds

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,14 @@
-const router = require('express').Router();
-
+const express = require('express');
+const router = express.Router();
 const users = ['Bob', 'Alex', 'Will', 'Tristan'];
 
 module.exports = (db) => {
-  // all routes will go here 
+  // all routes will go here
   router.get('/', (req, res) => {
-      res.json(users);
+
+    res.json(users);
+
   });
 
   return router;
-}
+};
