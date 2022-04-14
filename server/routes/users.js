3abe78402487cb2179bff8189bfbe7522e06a745
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-04-14 16:55:13 -0400

    init setup

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 const users = ['Bob', 'Alex', 'Will', 'Tristan'];
 
 module.exports = (db) => {
-  // all routes will go here 
+  // all routes will go here
   router.get('/', (req, res) => {
-      res.json(users);
+    res.json(users);
   });
 
   return router;
-}
+};
