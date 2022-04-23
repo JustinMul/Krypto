5d857646492c018fa7ae171bae140231cdd060a9
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-04-23 11:46:43 -0400

    changing chat js file

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,87 +1,91 @@
-import React, { useEffect, useState } from "react";
-import ScrollToBottom from "react-scroll-to-bottom";
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import Avatar from '@mui/material/Avatar';
- 
-function Chat({ socket, user, room, img }) {
- const [currentMessage, setCurrentMessage] = useState("");
- const [messageList, setMessageList] = useState([]);
- 
- const sendMessage = async () => {
-   if (currentMessage !== "") {
-     const messageData = {
-       img: img,
-       room: room,
-       author: user,
-       message: currentMessage,
-       time:
-         new Date(Date.now()).getHours() +
-         ":" +
-         new Date(Date.now()).getMinutes(),
-     };
- 
-     await socket.emit("send_message", messageData);
-     setMessageList((list) => [...list, messageData]);
-     setCurrentMessage("");
-   }
- };
- 
- useEffect(() => {
-   socket.on("receive_message", (data) => {
- 
-     setMessageList((list) => [...list, data]);
-    
-   });
- }, [socket]);
- 
- return (
-<div className="chat-window">
-     <div className="chat-header">
-       <p>Welcome to {room} chat</p>
-     </div>
-     <div className="chat-body">
-       <ScrollToBottom className="message-container">
-         {messageList.map((messageContent) => {
-           return (
-             <div
-               className="message"
-               id={user === messageContent.author ? "you" : "other"}
-             >
-               <div>
-                 <div className="message-content">
-                 <Avatar alt="Remy Sharp"
-                   src={messageContent.img}
-                   sx={{ width: 80, height: 80 }}
-                 />
-                 <p id="time">{messageContent.author} sent at: {messageContent.time}</p>
-                   <p><strong>{messageContent.message}</strong></p>
-                 </div>
-                 <div className="message-meta">
-                 </div>
-               </div>
-             </div>
-           );
-         })}
-       </ScrollToBottom>
-     </div>
-     <div className="chat-footer">
-       <input
-         type="text"
-         value={currentMessage}
-         placeholder="Hey..."
-         onChange={(event) => {
-           setCurrentMessage(event.target.value);
-         }}
-         onKeyPress={(event) => {
-           event.key === "Enter" && sendMessage();
-         }}
-       />
-       <Button onClick={sendMessage} variant="contained" endIcon={<SendIcon />}>
-       Send
-       </Button>
-     </div>
-   </div>
- );
-}
-export default Chat;
+ <Grid container 
+      spacing={2}
+      width="40vw"
+      height="80vh"
+      display="flex"
+      direction='column'
+      alignItems="center"
+      justifyContent="space-between"
+      paddingTop={2}
+      >
+        <Grid>
+            <Typography component="h1" variant="h4">
+              Welcome {user} to {room} Room
+            </Typography>
+            <Accordion >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography component="h1" variant="h6" align='center'>Chat Room Guidelines</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                Do not verbally abuse, attack, embarrass, or threaten anyone else in the chat room, no matter
+                what they might say to you.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+        </Grid>
+          <Grid overflow='hidden'>
+
+            <ScrollToBottom className="message-container">
+              {messageList.map((messageContent) => {
+                return (
+                  <div
+                  className="message"
+                  id={user === messageContent.author ? "you" : "other"}
+                >
+                        <Box mb={2}
+                        display="flex"
+                        flexDirection="column"
+                        style={{
+                          border: "1px solid black",
+                          overflow: "hidden",
+                          overflowY: "scroll" // added scroll
+                        }}>
+                          <Card sx={{ width: 300 , padding:0.5}}>
+                            <CardActionArea>
+                              <Chip
+                                avatar={<Avatar alt="Natacha" src={messageContent.img} />}
+                                label={messageContent.author}
+                                variant="outlined"
+                              />
+                              <CardContent>
+                                <Typography component="h8" variant="h8">sent at: {messageContent.time}</Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                  {messageContent.message}
+                                </Typography>
+                              </CardContent>
+                            </CardActionArea>
+
+                          </Card>
+                          </Box>
+                  </div>
+                );
+              })}
+            </ScrollToBottom>
+            
+          </Grid>
+      
+        <Grid item display='flex' justifyContent="space-between" sx={{ width: 600}}>
+            <TextField
+              type="text"
+
+              value={currentMessage}
+              inputProps={{style: {width: 400, height: 5}}} 
+              placeholder="Write your message here"
+              onChange={(event) => {
+              setCurrentMessage(event.target.value);
+              }}
+              onKeyPress={(event) => {
+                event.key === "Enter" && sendMessage();
+              }}
+            />
+            <Button onClick={sendMessage} size='medium' variant="contained" endIcon={<SendIcon />}>
+            Send
+            </Button>
+        </Grid>
+
+    </Grid>
