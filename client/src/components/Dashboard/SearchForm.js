a88f0099f0b77bb2c7986d17ce16ce502f233150
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-04-25 11:35:35 -0400

    prepare for pull

--- client/src/components/Dashboard/SearchForm.js
+++ client/src/components/Dashboard/SearchForm.js
@@ -9,7 +9,6 @@ const SearchForm = (props) => {
     <Grid container justifyContent="center">
     <form>
       <Input style={{width: 400, height: 40 }} 
-
       value={props.search}
       onChange={props.onChange} 
       name="Search Crypto" 
@@ -17,7 +16,6 @@ const SearchForm = (props) => {
       placeholder="Search Cryptos..." 
       startAdornment={<InputAdornment position="start"><SearchIcon style={(props.mode === "dark") ? {color: "white"} : {color: "black"}}/></InputAdornment>}
       />
-
     </form>
     </Grid>
   )
